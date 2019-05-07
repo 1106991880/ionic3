79a5fca3d624ad09d4017dd2565c9173b5256be2
yangzhibin <1106991880@qq.com>
2019-05-07 18:23:17 +0800

    新增调用百度地图进行路线规划导航功能。

--- src/pages/baidu-map/baidu-map.ts
+++ src/pages/baidu-map/baidu-map.ts
@@ -3,6 +3,7 @@ import {IonicPage, NavParams} from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import {NativeServiceProvider} from "../../providers/native-service/native-service";
 import { Storage } from "@ionic/storage";
+//import * as $ from '../../../node_modules/jquery/dist/jquery.js';
 /**
  * Generated class for the BaiduMapPage page.
  *
@@ -11,6 +12,8 @@ import { Storage } from "@ionic/storage";
  */
 declare var BMap;
 declare var baidumap_location;
+declare let appAvailability: any;
+declare let startApp: any;
 @IonicPage()
 @Component({
   selector: 'page-baidu-map',
@@ -39,6 +42,12 @@ export class BaiduMapPage {
               public storage:Storage,
               public navParams:NavParams) {
     this.city = navParams.get('city');
+
+    let that = this;
+    (<any>window).goMap = function (lng,lat) {
+      that.goMap(lng,lat);
+    }
+
   }
 
   ionViewDidLoad() {
@@ -48,8 +57,6 @@ export class BaiduMapPage {
       enableMapClick: true
     });
     //地址解析对象
-
-
 
 
 
@@ -128,7 +135,43 @@ export class BaiduMapPage {
         }
       });
 
-      //获取打点的详细信息
+      var opts = {
+        width : 250,     // 信息窗口宽度
+        height: 80,     // 信息窗口高度
+        title : "信息窗口" , // 信息窗口标题
+        enableMessage:true//设置允许信息窗发送短息
+      };
+
+      var markerArr = [];
+      //2019-05-07
+      local.setMarkersSetCallback(function(pois){
+        for(var i=0;i<pois.length;i++){
+          markerArr[i]=pois[i].marker;
+          //获取点的经纬度
+          //console.log("pois[i]-----"+pois[i]);
+          //console.log("pois[i]getPosition-----"+pois[i].marker.getPosition().lat);
+          //var marker = new BMap.Marker(new BMap.Point(,));  // 创建标注
+          //把点添加到地图上去
+          map.addOverlay(pois[i].marker);
+          pois[i].marker.addEventListener("infowindowopen", function(e){
+            //do something
+            //console.log("do something");
+            //console.log("e-------------"+e.target.getPosition().lng);
+            //console.log("e-------------"+e.target.getPosition().lat);
+            var p = e.target;
+            var positionLng = e.target.getPosition().lng;
+            var positionLat = e.target.getPosition().lat;
+            var point = new BMap.Point(p.getPosition().lng, p.getPosition().lat);
+            var infoWindow = new BMap.InfoWindow("<a onclick=\"goMap('"+positionLng+"','"+positionLat+"')\">到这去</a>",opts);  // 创建信息窗口对象
+            //也可以用下面的方式设置弹窗的内容
+            //infoWindow.setContent("<a>我是aaa</a>");
+            map.openInfoWindow(infoWindow,point);
+          })
+        }
+      })
+
+
+
 
 
       //默认查询新增社区服务中心查询
@@ -164,12 +207,12 @@ export class BaiduMapPage {
   }
       */
 
-
   //按钮查询
   medicalSearch() {
     var map = new BMap.Map(this.mapElement.nativeElement, {
       enableMapClick: true
     }); //创建地图实例
+
     //获取输入查询内容
     var condition = this.medical;
     console.log("condition"+condition);
@@ -178,15 +221,106 @@ export class BaiduMapPage {
       this.ionViewDidLoad() ;
     }
     map.centerAndZoom(this.city,11);
+
     var local = new BMap.LocalSearch(map, {
       renderOptions: {
         map: map,
         autoViewport: true,
         panel:"r-result"  //查询结果面板显示
+      },
+      onSearchComplete : function(results) {
+        //console.log(results);
       }
     });
+
+       var opts = {
+         width : 250,     // 信息窗口宽度
+         height: 80,     // 信息窗口高度
+         title : "信息窗口" , // 信息窗口标题
+         enableMessage:true//设置允许信息窗发送短息
+       };
+
+    var markerArr = [];
+    //2019-05-07
+       local.setMarkersSetCallback(function(pois){
+         for(var i=0;i<pois.length;i++){
+           markerArr[i]=pois[i].marker;
+           //获取点的经纬度
+           //console.log("pois[i]-----"+pois[i]);
+           //console.log("pois[i]getPosition-----"+pois[i].marker.getPosition().lat);
+           //var marker = new BMap.Marker(new BMap.Point(,));  // 创建标注
+           //把点添加到地图上去
+           map.addOverlay(pois[i].marker);
+           pois[i].marker.addEventListener("infowindowopen", function(e){
+             //do something
+             //console.log("do something");
+             //console.log("e-------------"+e.target.getPosition().lng);
+             //console.log("e-------------"+e.target.getPosition().lat);
+             var p = e.target;
+             var positionLng = e.target.getPosition().lng;
+             var positionLat = e.target.getPosition().lat;
+             var point = new BMap.Point(p.getPosition().lng, p.getPosition().lat);
+             var infoWindow = new BMap.InfoWindow("<a onclick=\"goMap('"+positionLng+"','"+positionLat+"')\">到这去</a>",opts);  // 创建信息窗口对象
+             //也可以用下面的方式设置弹窗的内容
+             //infoWindow.setContent("<a>我是aaa</a>");
+             map.openInfoWindow(infoWindow,point);
+           })
+         }
+       })
+
     //默认查询新增社区服务中心查询
-    local.searchNearby([condition], this.city, 10000);
+       local.searchNearby([condition], this.city, 10000);
+     }
+
+     goMap(lng,lat){
+       this.openMap(lng,lat);
+     }
+  openMap(lng,lat){
+    var that = this;
+    //alert("打开百度地图...");
+    var scheme = 'com.baidu.BaiduMap';
+    appAvailability.check(
+      scheme,       // URI Scheme or Package Name
+      function() {  // Success callback
+        //window.location.href = 'bdapp://map/direction?&origin=latlng:116.291226,39.965221|name:我的位置&destination=latlng:'+lat+','+lng+'&coord_type= bd09ll&src=andr.baidu.openAPIdemo';
+        window.location.href = 'bdapp://map/direction?&origin=latlng:116.291226,39.965221|name:我的位置&destination=latlng:'+lat+','+lng+'|name:目标位置&coord_type= bd09ll';
+      },
+      function() {  // Error callback
+        //alert(scheme + ' is not available :(');
+        that.nativeService.showToast("未安装百度地图App,无法进行路线规划");
+        //alert(11);
+      }
+    );
+  }
+
+
+
+    openMap1(lng,lat){
+    //百度地图参数配置
+    let baiduApp = startApp.set(
+      {
+        "action":"ACTION_VIEW",
+        "category":"CATEGORY_DEFAULT",
+        "type":"text/css",
+        "package":'com.baidu.BaiduMap',
+        "uri":"bdapp://map/direction?&origin=latlng:116.291226,39.965221|name:我的位置&destination=latlng:116.291226,39.965221|name:天安门&coord_type= bd09ll&src=andr.baidu.openAPIdemo",
+        "flags":["FLAG_ACTIVITY_CLEAR_TOP","FLAG_ACTIVITY_CLEAR_TASK"],
+        "intentstart":"startActivity",
+      }, { /* extras */
+        "EXTRA_STREAM":"extraValue1",
+        "extraKey2":"extraValue2"
+      }
+    );
+    baiduApp.start(function(){
+      //跳转到百度地图
+    },function(error){
+      //alert(error)
+      this.nativeService.showToast("未安装百度地图App");
+      //alert(1);
+    })
+  }
+
+
 
     /**
 
@@ -220,14 +354,12 @@ export class BaiduMapPage {
 
      */
 
-    /*map.addEventListener("click", function (e) {
-      alert(e.point.lng+"---"+e.point.lat);
-      //alert(e.point.lng+"---"+e.point.lat);
-      this.nativeService.showToast(e.point.lng+"---"+e.point.lat);
-    });*/
 
-  }
 
+
+  /**goMap = function (){
+    console.log("1231231");
+  };*/
 
   createTimeout(errorCallback, timeout) {
     var t = setTimeout(function () {
